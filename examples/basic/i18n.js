318a341f95dbd8fdeef96885ed2771f465f8c3a2
Alexander Kachkaev <alexander@kachkaev.ru>
2018-12-11 10:45:48 +0000

    Remove individual component creators to make the basic example smaller

--- examples/basic/i18n.js
+++ examples/basic/i18n.js
@@ -1,10 +1,6 @@
 const {
   createNextI18n,
   createNextI18nComponents,
-  // createLink,
-  // createWithNamespaces,
-  // createTrans,
-  // createAppI18nWrapper,
 } = require("@next-i18n/based-on-user");
 //= require("@next-i18n/based-on-path");
 //= require("@next-i18n/based-on-host");
@@ -20,11 +16,6 @@ const {
   Trans,
   withNamespaces,
 } = createNextI18nComponents(nextI18n);
-
-// const appI18nWrapper = createAppI18nWrapper(nextI18n);
-// const Link = createLink(nextI18n);
-// const Trans = createTrans(nextI18n);
-// const withNamespaces = createWithNamespaces(nextI18n);
 
 module.exports = {
   nextI18n,
