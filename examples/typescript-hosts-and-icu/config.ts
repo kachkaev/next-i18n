84799f7aefbe8a4d4b7aa2b6c9f01919bd6cd8b0
Alexander Kachkaev <alexander@kachkaev.ru>
2018-12-11 11:52:19 +0000

    Add a dummy secret to config.ts to demonstrate the security issue

--- examples/typescript-hosts-and-icu/config.ts
+++ examples/typescript-hosts-and-icu/config.ts
@@ -9,6 +9,9 @@ export const env = envalid.cleanEnv(
     HOST_RU: envalid.host({
       default: "ru.localhost",
     }),
+    SOME_SECRET_THAT_CANNOT_BE_SHARED: envalid.host({
+      default: "if this leaks to the client, the security is compromised",
+    }),
   },
   { strict: true },
 );
